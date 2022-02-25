873e70eb18579d9765ddb687d149cc962accb401
Shahryar Pirooz <dev.shahryar@gmail.com>
2022-02-25 08:19:55 +0000

    js changed

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);  
+    var year = new Date().getFullYear()+parseInt(years);
+    var result_text = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById('result').innerHTML= result_text;
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+'%';
 }
 
         
