be06e54ccb48f607fbf3b5836bc584327e5dece5
Shahryar Pirooz <dev.shahryar@gmail.com>
2022-02-25 09:05:35 +0000

    completed

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    var result_text = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-    document.getElementById('result').innerHTML= result_text;
+    //Add validation for "Principal" input box. If the user enters zero or negative values, show an alert "Enter a positive number"
+    if (principal == "0" || principal=="") {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        document.getElementById("principal").classList.add("invalid");
+    } else {
+        /*
+        When "Compute Interest" is clicked, set its inner html property to the below text.
+            If you deposit 1000000,
+            at an interest rate of 3.5%.
+            You will receive an amount of 175000,
+            in the year 2025
+        */
+        document.getElementById("principal").classList.remove("invalid");
+        var result_text = "If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+        document.getElementById("result").innerHTML= result_text;
+    }
+    
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval+'%';
 }
-
-        
